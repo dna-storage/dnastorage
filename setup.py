84a26739b92308ab0eb8824e07601a2b27057a14
James Tuck <jtuck@ncsu.edu>
2021-03-26 17:20:59 -0400

    move version forward

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dnastorage", 
-    version="0.9.1",
+    version="0.9.2",
     author="James M. Tuck",
     author_email="james.m.tuck@gmail.com",
     description="dnastorage provides basic support for encoding and decoding files into and out of DNA with several convenient interfaces.",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Lesser GNU General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
     ],
