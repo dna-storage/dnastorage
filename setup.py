1d1b395ca1cd5c6ef3f09ce727bd48fa0a71103e
James Tuck <15042082+jamesmtuck@users.noreply.github.com>
2021-05-06 18:17:17 -0400

    Update setup.py
    Fix license classifier.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Lesser GNU General Public License v3 (LGPLv3)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
     ],
